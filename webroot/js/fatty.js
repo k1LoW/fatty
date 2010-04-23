14f9fdeb0aaefd3028e5d070ee9a85f6a09489c5
k1LoW <k1lowxb@gmail.com>
2010-04-23 21:30:14 +0900

    Copy files for CakePHP 1.2

---
+++ webroot/js/fatty.js
@@ -0,0 +1,27 @@
+$(function(){
+      var a = null;
+      var b = null;
+      $('.draggable').draggable({
+                                    start: function(event, ui) {
+                                        var $self = $(this);
+                                        a = $(this).attr('id');
+                                        $self.css({zIndex:10});
+                                        $self.siblings('.draggable').css({zIndex:9});
+                                    },
+                                    end: function(event, ui) {
+                                        a = null;
+                                    },
+                                    axis: 'y',
+                                    revert: true,
+                                    opacity: 0.7
+                                });
+      $('.droppable').droppable({
+                                    greedy: true,
+                                    hoverClass: 'dropHover',
+                                    drop: function(event, ui) {
+                                        var $self = $(this);
+                                        b = $(this).attr('id');
+                                        location.href = 'diff/' + a + '/' + b;
+                                    }
+                                });
+  });
